af3d0277501b478903d5e1bf8062fcea93b706ed
SusantoMandal <86454870+SusantoMandal@users.noreply.github.com>
2023-05-23 01:14:42 +0530

    Added Leetcode Solution 347

---
+++ src/main/java/Problem347/Main.java
@@ -0,0 +1,33 @@
+package src.main.java.Problem347;
+
+import java.util.*;
+
+public class Main {
+    public int[] topKFrequent(int[] nums, int k) {
+        if(k == nums.length) return nums;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num: nums) {
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        List<Integer>[] bucket = new List[nums.length+1];
+        for(int i=0; i<bucket.length; i++) {
+            bucket[i] = new ArrayList<>();
+        }
+        for(int key: map.keySet()) {
+            bucket[map.get(key)].add(key);
+        }
+        List<Integer> li = new ArrayList<>();
+        for(int i=bucket.length-1; i>=0; i--) {
+            li.addAll(bucket[i]);
+        }
+        int[] ans = new int[k];
+        for(int i=0; i<k; i++) {
+            ans[i] = li.get(i);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Main ob = new Main();
+        System.out.println(Arrays.toString(ob.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2)));
+    }
+}
