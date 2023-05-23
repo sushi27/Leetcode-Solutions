72f2486973a97f501ed1729999ed950b323b198d
SusantoMandal <86454870+SusantoMandal@users.noreply.github.com>
2023-05-23 18:43:04 +0530

    Solution for Daily Challenge - Problem no. 703

---
+++ src/main/java/Problem703/Solution.java
@@ -0,0 +1,30 @@
+package src.main.java.Problem703;
+
+import java.util.PriorityQueue;
+
+class KthLargest {
+    int k;
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for(int num: nums) {
+            pq.add(num);
+        }
+    }
+
+    public int add(int val) {
+        pq.add(val);
+        while(pq.size() != k) pq.poll();
+        return pq.peek();
+    }
+}
+public class Solution {
+    public static void main(String[] args) {
+        KthLargest ob = new KthLargest(3, new int[]{4,5,8,2});
+        System.out.println(ob.add(3));
+        System.out.println(ob.add(5));
+        System.out.println(ob.add(10));
+        System.out.println(ob.add(9));
+        System.out.println(ob.add(4));
+    }
+}
